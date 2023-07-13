b6d565c96cfb18fe39360ab3b0de5fb4573b00c7
JBuckley2005 <97849270+JBuckley2005@users.noreply.github.com>
2023-07-13 15:41:15 +0100

    Seperated the code into smaller, more percise functions and error handling for when no command line arguments are given

--- Project.js
+++ Project.js
@@ -7,28 +7,43 @@ const client = new speech.SpeechClient({
   keyFilename: keyFilename
 });
 
-async function getText() {
-    let filePathAddress = process.argv[2];
-    filePath = path.join(__dirname, filePathAddress);
-    audioBytes = fs.readFileSync(filePath);
-    const audio = {
-        content: audioBytes.toString('base64'),
-    };
-    const config = {
-        encoding: 'FLAC',
-        languageCode: 'en-UK',
-    };
-    const request = {
-        audio: audio,
-        config: config,
-    };
-
-    const [response] = await client.recognize(request);
-    const transcription = response.results
-        .map(result => result.alternatives[0].transcript)
-        .join('\n');
-    console.log(`Transcription: ${transcription}`);
+function getFileAsBytes() {
+  let filePathAddress = process.argv[2];
+  let filePath = path.join(__dirname, filePathAddress);
+  let audioBytes = fs.readFileSync(filePath);
+  return audioBytes;
 }
 
-getText();
+async function getText(audioBytes) {
+  const audio = {
+    content: audioBytes.toString('base64'),
+  };
+  const config = {
+    encoding: 'FLAC',
+    languageCode: 'en-UK',
+  };
+  const request = {
+    audio: audio,
+    config: config,
+  };
+
+  const [response] = await client.recognize(request);
+  const transcription = response.results
+    .map(result => result.alternatives[0].transcript)
+    .join('\n');
+  return transcription;
+}
+
+async function main() {
+  if (!process.argv[2]) {
+    console.log("A filepath to an audio file is required as a command line argument");
+  } else {
+    let audioBytes = getFileAsBytes();
+    let text = await getText(audioBytes);
+    console.log(`Transcription: ${text}`);
+  }
+}
+
+main();
+
 
