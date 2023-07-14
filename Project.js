205fca53a4f89375c477b0344624471cbb439e23
JBuckley2005 <97849270+JBuckley2005@users.noreply.github.com>
2023-07-14 16:07:23 +0100

    Added the functionality for all text to be stored to an sqlite database along with the inputted audio and time created

--- Project.js
+++ Project.js
@@ -1,16 +1,38 @@
+const sqlite3 = require('sqlite3')
 const express = require('express');
 const app = express();
 const PORT = 8040;
 const speech = require('@google-cloud/speech');
 const path = require('path');
 const fs = require('fs');
-const base64Regex = "/^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/";
+//const base64Regex = "/^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/";
 
 app.use(express.json({limit: '100mb'}));
 const keyFilename = path.join(__dirname, 'key.json');
 const client = new speech.SpeechClient({
   keyFilename: keyFilename
 });
+
+async function connectToDatabase() {
+  let db = new sqlite3.Database('./SpeechToText.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      return db;
+    }
+  });
+  return db;
+}
+
+async function insertEntry(text, audioBytes, db){
+  const date = new Date();
+  let dateAdded = date.toLocaleDateString();
+  db.run('INSERT INTO Entries (text, inputtedAudio, dateAdded) VALUES (?,?,?)', [text, audioBytes, dateAdded], function(err) {
+    if (err){
+      return false;
+    } else {
+      return true;
+    }
+  })
+}
 
 async function getText(audioBytes) {
     const config = {
@@ -37,11 +59,9 @@ app.listen(
 app.post('/get_text', async (req, res) => {
   const {audio} = req.body;
   if(!audio || !audio.data) {
-    return res.status(418).send({message: "A FLAC audio file is required as input"})
+    return res.send(418, {message: "A FLAC audio file is required as input"})
   }
-
   try {
-    let audioBytes = audio.data;
     // This would test if the inputted file was 64 bit encoded but the regex test takes too long 
     // if (base64Regex.test(audioBytes)){
     //    let text = await getText(audioBytes);
@@ -50,10 +70,18 @@ app.post('/get_text', async (req, res) => {
     // } else {
     //   return res.status(500, {message:"The file pass was not base 64 encoded"});
     // }
-    let text = await getText(audioBytes);
-    console.log("Text result:", text);
-    res.send({message: "Wayhey!"})
+    let text = await getText(audio.data);
+    let db = await connectToDatabase();
+    if(!db){
+      return res.send(500, {message:"Failed to connect to database"});
+    }
+    if(insertEntry(text, audio.data, db)){
+      return res.send(200, {message: "Wayhey!"})
+    } else {
+      return res.send(500, {message:"Failed to insert entry into database"});
+    }
   } catch {
-    return res.status(500, {message:"An error has occurred"});
+    return res.send(500, {message:"An error has occurred"});
   }
-})
+}
+)
