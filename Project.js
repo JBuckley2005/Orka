d014298395eb5ce0ef74d739a3c0e17f6e178800
JBuckley2005 <97849270+JBuckley2005@users.noreply.github.com>
2023-07-14 12:00:05 +0100

    Created a local endpoint, allowing the user to pass a based 64 encoded file as a post request instead of a command line argument

--- Project.js
+++ Project.js
@@ -1,49 +1,62 @@
+const express = require('express');
+const app = express();
+const PORT = 8040;
 const speech = require('@google-cloud/speech');
 const path = require('path');
 const fs = require('fs');
+const base64Regex = "/^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/";
 
+app.use(express.json({limit: '100mb'}));
 const keyFilename = path.join(__dirname, 'key.json');
 const client = new speech.SpeechClient({
   keyFilename: keyFilename
 });
 
-function getFileAsBytes() {
-  let filePathAddress = process.argv[2];
-  let filePath = path.join(__dirname, filePathAddress);
-  let audioBytes = fs.readFileSync(filePath);
-  return audioBytes;
-}
-
 async function getText(audioBytes) {
-  const audio = {
-    content: audioBytes.toString('base64'),
-  };
-  const config = {
-    encoding: 'FLAC',
-    languageCode: 'en-UK',
-  };
-  const request = {
-    audio: audio,
-    config: config,
-  };
-
-  const [response] = await client.recognize(request);
-  const transcription = response.results
-    .map(result => result.alternatives[0].transcript)
-    .join('\n');
-  return transcription;
+    const config = {
+      encoding: 'FLAC',
+      languageCode: 'en-UK',
+    };
+    const request = {
+      audio: {content : audioBytes},
+      config: config,
+    };
+    
+    const [response] = await client.recognize(request);
+    const transcription = response.results
+      .map(result => result.alternatives[0].transcript)
+      .join('\n');
+    return transcription;
 }
 
-async function main() {
-  if (!process.argv[2]) {
-    console.log("A filepath to an audio file is required as a command line argument");
-  } else {
-    let audioBytes = getFileAsBytes();
-    let text = await getText(audioBytes);
-    console.log(`Transcription: ${text}`);
+app.listen(
+  PORT, 
+  () => console.log('Its alive on https://localhost:8040')
+)
+
+app.post('/get_text', async (req, res) => {
+  const {audio} = req.body;
+  if(!audio || !audio.data) {
+    return res.status(418).send({message: "A FLAC audio file is required as input"})
   }
-}
 
-main();
-
-
+  try {
+    let audioBytes = audio.data;
+    console.log(audioBytes);
+    if (base64Regex.test(audioBytes)){
+      console.log("H");
+    } else {
+      return res.status(500, {message:"An error has occurred"});
+    }
+    // try {
+      
+    // }catch(e) {
+    //   return res.status(500, {message:"An error has occurred"});
+    // }
+    let text = await getText(audioBytes);
+    console.log("Text result:", text);
+    res.send({message: "Wayhey!"})
+  } catch {
+    return res.status(500, {message:"An error has occurred"});
+  }
+})
